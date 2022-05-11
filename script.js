0414f0d5e6c9fdcbf1623dd4480a43b787318268
Vishnu <58354036+Visque@users.noreply.github.com>
2022-05-11 22:32:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const header = document.getElementById("header")
+
+window.onresize = () => {
+    console.log("hello  resizer")
+    
+    let a = innerWidth
+    console.log("log check width: ", a)
+    
+    let toatle = screen.width
+    
+    
+    console.log(toatle / 10, a, toatle / 2);
+
+
+    if (a <= toatle / 2 && a >= toatle / 10) {
+        console.log("yellow");
+        header.classList.add("test");
+        header.style.setProperty("--element-height", a);
+    }
+    else if (a <= toatle / 2 && a >= toatle / 10) {
+      console.log("black");
+      header.classList.remove("test");
+      header.style.setProperty("--element-height", a);
+    }
+
+}
+
+
+{
+    number: [idx, ]
+}
